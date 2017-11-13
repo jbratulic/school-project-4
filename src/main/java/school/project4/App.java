89350f4eb8369029c422f8fa78c1db63cd8947b8
jbratulic <jbratulic@protonmail.com>
2017-11-13 21:02:41 +0100

    res1

---
+++ src/main/java/school/project4/App.java
@@ -0,0 +1,13 @@
+package school.project4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
